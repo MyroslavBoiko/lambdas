a39ee09ae4799fe97735652327176bc7e5aeea6d
Myroslav Boiko <miros.blom@gmail.com>
2024-01-25 10:45:57 +0200

    Task03 remove version from lambda

--- task03/jsrc/main/java/com/task03/HelloWorld.java
+++ task03/jsrc/main/java/com/task03/HelloWorld.java
@@ -1,15 +1,14 @@
 package com.task03;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @LambdaHandler(lambdaName = "hello_world",
-	roleName = "hello_world-role",
-	isPublishVersion = true
+		roleName = "hello_world-role"
 )
 public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
