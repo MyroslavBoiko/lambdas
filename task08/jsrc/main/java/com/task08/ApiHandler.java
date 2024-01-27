1e91716fbceac504f2db96ffa689cf1c7ea59bcc
Myroslav Boiko <miros.blom@gmail.com>
2024-01-27 22:49:11 +0200

    Task08 Lambda layers

---
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -0,0 +1,51 @@
+package com.task08;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.Gson;
+import com.syndicate.deployment.annotations.LambdaUrlConfig;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
+import com.syndicate.deployment.model.ArtifactExtension;
+import com.syndicate.deployment.model.DeploymentRuntime;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+
+@LambdaHandler(lambdaName = "api_handler",
+		roleName = "api_handler-role",
+		layers = {"sdk-layer"}
+)
+@LambdaLayer(
+		layerName = "sdk-layer",
+		libraries = {"lib/commons-lang3-3.14.0.jar", "lib/gson-2.10.1.jar", "lib/sdk-1.4.0.jar"},
+		runtime = DeploymentRuntime.JAVA8,
+		artifactExtension = ArtifactExtension.ZIP
+)
+@LambdaUrlConfig(
+		authType = AuthType.NONE,
+		invokeMode = InvokeMode.BUFFERED
+)
+public class ApiHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+	private static final int SC_OK = 200;
+	private static final int SC_BAD_REQUEST = 400;
+	private final Gson gson = new Gson();
+
+	@Override
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent,
+													  Context context) {
+
+		context.getLogger().log(apiGatewayProxyRequestEvent.toString());
+		try {
+			return new APIGatewayProxyResponseEvent()
+					.withStatusCode(SC_OK)
+					.withBody(OpenMeteoApi.getWeatherForecast());
+		} catch (RuntimeException exception) {
+			return new APIGatewayProxyResponseEvent()
+					.withStatusCode(SC_BAD_REQUEST)
+					.withBody(gson.toJson(new ErrorMessage(exception.getMessage())));
+		}
+	}
+}
---
+++ task08/jsrc/main/java/com/task08/ErrorMessage.java
@@ -0,0 +1,9 @@
+package com.task08;
+
+public class ErrorMessage {
+    private final String error;
+
+    public ErrorMessage(String error) {
+        this.error = error;
+    }
+}
